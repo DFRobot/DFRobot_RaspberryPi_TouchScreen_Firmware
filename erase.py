f0149534d18a32cce465592f40b50c59830683ac
wxzed <xiao.wu@dfrobot.com>
2018-03-15 16:31:41 +0800

    first commit 20180315

---
+++ erase.py
@@ -0,0 +1,18 @@
+import os
+def run():
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08008000 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08008400 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08008800 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08008C00 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08009000 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08009400 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08009800 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x08009C00 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800A000 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800A400 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800A800 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800AC00 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800B000 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800B400 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800B800 -D erase.bin")
+    os.system("dfu-util -d 0483:df11 -a 0 -s 0x0800BC00 -D erase.bin")
